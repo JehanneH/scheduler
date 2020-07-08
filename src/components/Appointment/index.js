ce78a92ef9b117352ba16a7359180642766711ba
Jehanne Hodge <samarahjhodge@gmail.com>
2020-07-08 18:22:00 -0400

    add show and empty to the appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,16 @@ import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
   return (
-  <article className="appointment"></article>
+  <article className="appointment">
+    <Header time={props.time}/>
+    {props.interview ? 
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete}
+        /> : <Empty />}
+    
+  </article>
   )
 }
