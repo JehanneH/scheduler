4e564f081b5865350fdcca94caf835e6099d0dc6
Jehanne Hodge <samarahjhodge@gmail.com>
2020-07-09 22:55:38 -0400

    create custom hook and tests

--- src/components/Application.js
+++ src/components/Application.js
@@ -40,8 +40,9 @@ export default function Application(props) {
   const interviewersPromise = axios.get(`/api/interviewers`)
 
   Promise.all([daysPromise, appointmentsPromise, interviewersPromise]).then((all) => {
-    // console.log("------>", all[2].data)
+
     setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
+
     });
   }, []);
 
