15833f7eda635a1485fc898e91695fe75db9d1b8
Jehanne Hodge <samarahjhodge@gmail.com>
2020-07-14 16:33:51 -0400

    add second test to application tests and add mock axios put

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitForElement, prettyDOM } from "@testing-library/react";
 
-import { getByText } from "@testing-library/react";
+import { getByText, getAllByTestId, getByPlaceholderText, getByAltText } from "@testing-library/react";
+
 
 
 import Application from "components/Application";
@@ -10,18 +11,36 @@ import Application from "components/Application";
 afterEach(cleanup);
 
 describe("Application", () => {
-it("changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
 
-  await waitForElement(() => getByText("Monday"));
+    await waitForElement(() => getByText("Monday"));
 
-  fireEvent.click(getByText("Tuesday"));
+    fireEvent.click(getByText("Tuesday"));
 
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+   expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 
-it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container, debug } = render (<Application />); 
 
-})
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+ 
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment));
+  
+  })
 
 })
