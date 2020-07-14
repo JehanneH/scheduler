c4e3e312e0cd008ecefddc6c1a60d74ed33ba4d9
Jehanne Hodge <samarahjhodge@gmail.com>
2020-07-14 15:14:44 -0400

    create mock data and axios get request for Application test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,27 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
+
+import { getByText } from "@testing-library/react";
+
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe("Application", () => {
+it("changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
+
+it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+
+})
+
+})
